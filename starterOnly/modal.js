3c8a6f1bc6c69b309e7208c61976051f5774d025
redoneElli <redwane.bouselham@editions-ellipses.fr>
2022-11-20 23:43:17 +0100

    add : validation du form part echec

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,11 +20,16 @@ const subBtn = document.querySelector(".btn-submit");
 const subBtnClose = document.querySelector(".btn-sub-close");
 
 // Form in modal
-const textFormFirst = document.querySelector("#first");
-const textFormLast = document.querySelector("#last");
-const emailForm = document.querySelector("#email");
-const birthForm = document.querySelector("#birthdate");
-const quantityForm = document.querySelector("#quantity");
+const form = {
+  firstname: document.querySelector("#first"),
+  lastname: document.querySelector("#last"),
+  email: document.querySelector("#email"),
+  birthdate: document.querySelector("#birthdate"),
+  quantEvent: document.querySelector("#quantity"),
+  locEvent: document.querySelector("#location1"),
+  userCondition: document.querySelector("#checkbox1")
+}
+
 
 // Checkbox in form
 const loc1Form = document.querySelector("#location1");
@@ -34,9 +39,18 @@ const loc4Form = document.querySelector("#location4");
 const loc5Form = document.querySelector("#location5");
 const loc6Form = document.querySelector("#location6");
 const labelRadio = document.querySelector(".text-label");
-const checkbox1 = document.querySelector('#checkbox1');
 
+let errors = [];
 
+const errorText = {
+  firstname: 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.',
+  lastname: 'Veuillez entrer 2 caractères ou plus pour le champ du nom.',
+  email: 'Veuillez entrer une adresse email valide.',
+  birthdate: 'Veuillez renseigner votre date de naissance.',
+  quantEvent: 'Veuillez entrer une valeur numérique.',
+  locEvent: 'Vous devez choisir une option.',
+  userCondition: 'Vous devez vérifier que vous acceptez les termes et conditions.'
+}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -47,17 +61,10 @@ modalClose.addEventListener("click", closeModal);
 
 // completion control of the form before submit
 formReserve.addEventListener("submit", (e) => {
+  // Avoid form submit
   e.preventDefault();
 
-  let checkTextForm = (textFormFirst.value.length > 2 && textFormLast.value.length > 2);
-  let checkMailForm = emailForm.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
-  let checkBirthForm = birthForm.value != '';
-  let checkQuantityForm = quantityForm.value != '';
-  let checkRadioLocForm = (loc1Form.checked || loc2Form.checked || loc3Form.checked || loc4Form.checked || loc5Form.checked || loc6Form.checked);
-  let checkCheckbox = checkbox1.checked;
-
-
-  if (checkTextForm && checkMailForm && checkBirthForm && checkQuantityForm && checkRadioLocForm && checkCheckbox) {
+  if (checkForm()) {
     // Affichage message réussite
     validationSuccess();
   } else {
@@ -78,7 +85,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal function
+// Close the modal form and submit if it's complete
 function closeModal() {
   if (subBtn.classList.contains('btn-sub-close')) {
     formReserve.submit();
@@ -88,7 +95,37 @@ function closeModal() {
 
 }
 
-// Affichage réussite function 
+// Vérifie que chacun des champs soit complété correctement
+function checkForm() {
+  let checks = {
+    firstname: form['firstname'].value.length > 2,
+    lastname: form['lastname'].value.length > 2,
+    email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(form['email'].value),
+    birthdate: form['birthdate'].value != '',
+    quantEvent: form['quantEvent'].value != '',
+    locEvent: (loc1Form.checked || loc2Form.checked || loc3Form.checked || loc4Form.checked || loc5Form.checked || loc6Form.checked),
+    userCondition: form['userCondition'].checked
+  };
+
+  /* console.log(form['userCondition']);
+  console.log(checks); */
+  console.log(checks['firstname'] && checks['lastname'] && checks['email'] && checks['birthdate'] && checks['quantEvent'] && checks['locEvent'] && checks['userCondition']);
+
+
+  if (checks['firstname'] && checks['lastname'] && checks['email'] && checks['birthdate'] && checks['quantEvent'] && checks['locEvent'] && checks['userCondition']) {
+    return true;
+  } else {
+    errors = [];
+    for (let check in checks) {
+      if (!checks[check]) {
+        errors.push(check);
+      }
+    }
+    return false;
+  }
+
+}
+
 function validationSuccess() {
   // Masquer le form
   formData.forEach((formData) => {
@@ -100,19 +137,12 @@ function validationSuccess() {
 
   messageValidation();
 
-  // Changer le text du btn + add la classe btn-sub-close
+  // Change text of btn + add class 'btn-sub-close' 
   subBtn.value = 'Fermer';
   subBtn.classList.add('btn-sub-close');
 }
 
-// affichage échec function 
-function validationFailure() {
-
-  console.log('nokk');
-}
-
-
-// Faire apparaitre le text de validation
+// Create and appear validation message
 function messageValidation() {
   let textValid = document.createElement('p');
   textValid.textContent = 'Merci pour votre inscription';
@@ -125,3 +155,26 @@ function messageValidation() {
   textValid.style.textAlign = 'center';
   modalbdy.prepend(textValid);
 }
+
+// affichage échec function 
+function validationFailure() {
+  console.log('nokk');
+
+  /* console.log(errors); */
+
+  for (let prop in form) {
+    if (form[prop].parentNode.hasAttribute("data-error")) {
+      form[prop].parentNode.removeAttribute("data-error");
+      form[prop].parentNode.removeAttribute("data-error-visible");
+    }
+    errors.forEach(err => {
+      if (err == prop) {
+        // ajouter l'attribut d'erreur et le text error
+        form[prop].parentNode.setAttribute("data-error", errorText[prop]);
+        form[prop].parentNode.setAttribute("data-error-visible", "true");
+      }
+    })
+  }
+
+}
+
